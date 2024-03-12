0d48624e191d5ffefa5c49f1cbfc318847436704
Emanuel <emanuel.lpereira@gmail.com>
2024-03-11 23:31:12 -0300

    correcting bugs

--- pages/index.js
+++ pages/index.js
@@ -178,4 +178,3 @@ const Home = () => (
 )
 
 export default Home
-export { getServerSideProps } from '../components/chakra'
--- pages/works.js
+++ pages/works.js
@@ -92,4 +92,3 @@ const Works = () => (
 )
 
 export default Works
-export { getServerSideProps } from '../components/chakra'
